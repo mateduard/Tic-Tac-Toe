61b7e7bf6e18cdfaf48a11968cc89222ae93c3fe
mateduard <edy_angel_2008@yahoo.com>
2023-10-10 14:36:56 +0300

    Fixed end of game Draw bug

--- script.js
+++ script.js
@@ -10,81 +10,79 @@ let boxes = [0, 3, 4, 5, 6, 7, 8, 9, 10];
 gameBox.addEventListener('click', nextMove);
 
 function nextMove(e) {
-    createGameElement(e);
-    checkEndOfGame();
-    checkEndOfRounds();
+  createGameElement(e);
+  checkEndOfGame();
+//   checkEndOfRounds();
 }
 
 function createGameElement(e) {
-    if (moveCounter % 2 === 0) {
-        if (!e.target.style.backgroundImage) {
-            e.target.style.backgroundImage = 'url(circle.jpg)';
-            let elId = (e.target.id).slice(1);
-            boxes[elId] = 2;
-            moveCounter++;
-        }
+  if (moveCounter % 2 === 0) {
+    if (!e.target.style.backgroundImage) {
+      e.target.style.backgroundImage = 'url(circle.jpg)';
+      let elId = e.target.id.slice(1);
+      boxes[elId] = 2;
+      moveCounter++;
     }
-    else {
-        if (!e.target.style.backgroundImage) {
-            e.target.style.backgroundImage = 'url(cross.jpg)';
-            let elId = (e.target.id).slice(1);
-            boxes[elId] = 1;
-            moveCounter++;
-        }
+  } else {
+    if (!e.target.style.backgroundImage) {
+      e.target.style.backgroundImage = 'url(cross.jpg)';
+      let elId = e.target.id.slice(1);
+      boxes[elId] = 1;
+      moveCounter++;
     }
+  }
 }
 
 function checkEndOfRounds() {
-    if (moveCounter > 9) {
-        moveCounter = 1;
-        displayEnd(0);
-    }
+  if (moveCounter > 9) {
+    moveCounter = 1;
+    displayEnd(0);
+  }
 }
 
 function checkEndOfGame() {
-    if (
-        boxes[0] === boxes[4] && boxes[0] === boxes[8] ||
-        boxes[0] === boxes[1] && boxes[1] === boxes[2] ||
-        boxes[0] === boxes[3] && boxes[3] === boxes[6] ||
-        boxes[1] === boxes[4] && boxes[4] === boxes[7] ||
-        boxes[3] === boxes[4] && boxes[4] === boxes[5] ||
-        boxes[2] === boxes[4] && boxes[4] === boxes[6] ||
-        boxes[8] === boxes[5] && boxes[5] === boxes[2] ||
-        boxes[8] === boxes[7] && boxes[7] === boxes[6]
-
-    ) {
-        if (moveCounter % 2 === 0) {
-            displayEnd(1);
-        } else {
-            displayEnd(2);
-        }
+  if (
+    (boxes[0] === boxes[4] && boxes[0] === boxes[8]) ||
+    (boxes[0] === boxes[1] && boxes[1] === boxes[2]) ||
+    (boxes[0] === boxes[3] && boxes[3] === boxes[6]) ||
+    (boxes[1] === boxes[4] && boxes[4] === boxes[7]) ||
+    (boxes[3] === boxes[4] && boxes[4] === boxes[5]) ||
+    (boxes[2] === boxes[4] && boxes[4] === boxes[6]) ||
+    (boxes[8] === boxes[5] && boxes[5] === boxes[2]) ||
+    (boxes[8] === boxes[7] && boxes[7] === boxes[6])
+  ) {
+    if (moveCounter % 2 === 0) {
+      displayEnd(1);
+    } else {
+        displayEnd(2);
     }
-}
+    } else {
+        checkEndOfRounds();
+    }
+  }
 
 function displayEnd(winner) {
-    endCover.style.display = 'flex';
-    const endBox = document.querySelector('#end-box');
+  endCover.style.display = 'flex';
+  const endBox = document.querySelector('#end-box');
 
-    if (winner === 0) {
-        endBox.innerHTML = `<h1>DRAW</h1>`;
-        endCover.addEventListener('click', resetGame);
-    } else {
-        const image = winner === 1 ? 'cross.jpg' : 'circle.jpg';
-        endBox.innerHTML = `
+  if (winner === 0) {
+    endBox.innerHTML = `<h1>DRAW</h1>`;
+    endCover.addEventListener('click', resetGame);
+  } else {
+    const image = winner === 1 ? 'cross.jpg' : 'circle.jpg';
+    endBox.innerHTML = `
         <h1>PLAYER ${winner} WINS</h1>
-        <img src="${image}" alt="cross">`
-        endCover.addEventListener('click', resetGame);
-    }
+        <img src="${image}" alt="cross">`;
+    endCover.addEventListener('click', resetGame);
+  }
 }
 
 function resetGame() {
+  boxes = [0, 3, 4, 5, 6, 7, 8, 9, 10];
+  document.querySelectorAll('.small-box').forEach((box) => {
+    box.style.backgroundImage = '';
+  });
+  moveCounter = 1;
 
-    boxes = [0, 3, 4, 5, 6, 7, 8, 9, 10];
-    document.querySelectorAll('.small-box')
-        .forEach(box => {
-            box.style.backgroundImage = '';
-        });
-    moveCounter = 1;
-
-    endCover.style.display = 'none';
+  endCover.style.display = 'none';
 }
